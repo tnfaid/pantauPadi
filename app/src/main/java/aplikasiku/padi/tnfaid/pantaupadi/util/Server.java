d74a49ab90d3e507c257adb62e6327312697f1fc
tnfaid <nurfaidahtitik@gmail.com>
2019-05-19 23:14:41 +0700

    update url api

--- app/src/main/java/aplikasiku/padi/tnfaid/pantaupadi/util/Server.java
+++ app/src/main/java/aplikasiku/padi/tnfaid/pantaupadi/util/Server.java
@@ -1,5 +1,8 @@
 package aplikasiku.padi.tnfaid.pantaupadi.util;
 
 public class Server {
-    public static final String URL = "https://pantaupadi.000webhostapp.com/webService/";
+//    public static final String URL = "https://pantaupadi.000webhostapp.com/webService/"; jikamenggunakan webhost
+//    alamat ip semisal 192.168.14.1/webService ini untuk load lgsung dari hape jadi ip nya disamakan
+public static final String URL = "http:/192.168.14.1:8080/webService/";
+
 }
