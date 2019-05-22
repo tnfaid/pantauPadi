8f76928c35176284861de650c5b213fdf20cb128
tnfaid <nurfaidahtitik@gmail.com>
2019-05-22 23:35:44 +0700

    bottom navigation

---
+++ app/src/main/java/aplikasiku/padi/tnfaid/pantaupadi/Fragment/tentang.java
@@ -0,0 +1,22 @@
+package aplikasiku.padi.tnfaid.pantaupadi.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import aplikasiku.padi.tnfaid.pantaupadi.R;
+
+public class tentang extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_tentang, container,false);
+        return view;
+    }
+}
